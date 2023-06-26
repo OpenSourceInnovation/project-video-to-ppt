d57de2dd988b70f983cae8589f121b4d6aa17c38
saicharankandukuri <saicharankandukuri1x1@gmail.com>
2023-06-26 17:11:36 +0530

    chunker: new chunker implementation to split texts

--- main.py
+++ main.py
@@ -3,26 +3,7 @@ from subtitles import getSubsText
 from models.distilbart_cnn_12_6 import summarize
 from models.t5_small_medium_title_generation import t5model as generate_title
 from marp_wrapper import marp
-
-def chunker(seq, size=1000):
-    words = seq.split()
-    chunks = []
-    current_chunk = ""
-    for word in words:
-        if len(current_chunk) + len(word) + 1 <= size:
-            current_chunk += f"{word} "
-        else:
-            chunks.append(current_chunk.strip())
-            current_chunk = f"{word} "
-    if current_chunk:
-        chunks.append(current_chunk.strip())
-    return chunks
-
-def chunklen(seq):
-    count = 0
-    for c in seq:
-        count += 1
-    return count
+from utils.chunk import LangChainChunker
 
 # Intermediary Markdown file
 print("Creating Markdown file...")
@@ -43,12 +24,13 @@ if __name__ == "__main__":
 print("Getting subtitles...")
 video_subs = getSubsText(opts.video)
 
-# divide the subs into chunks for more accurate summarization
-# TODO: divide the subs into chunks based on the topics
-# summarize each chunk and add it to the markdown file
-chunk_len = copy.deepcopy(chunklen(chunker(video_subs, size=200)))
+chunker_init = LangChainChunker(video_subs)
+chunks = chunker_init.chunker(size=200)
+chunk_len = len(chunks)
+print(f"subtitles divided to {chunk_len} chunks")
+
 chunk_num = 1
-for chunk in chunker(video_subs, size=200):
+for chunk in chunks:
     print(f"processing Chunk: {chunk_num}/{chunk_len}")
     summary = summarize(chunk)
     print("\t summary done..")
