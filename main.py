29e52f3b645182ff87ccbf916e24103e98b40ad8
saicharankandukuri <saicharankandukuri1x1@gmail.com>
2023-06-26 17:32:32 +0530

    main.py: add option to manually set chunk size

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from models.t5_small_medium_title_generation import t5model as generate_title
 from marp_wrapper import marp
 from utils.chunk import LangChainChunker
 
+CHUNK_SIZE  =   512
 # Intermediary Markdown file
 print("Creating Markdown file...")
 md = marp("summary.md")
@@ -12,20 +13,22 @@ md.header()
 
 if __name__ == "__main__":
     optparser = optparse.OptionParser()
-    optparser.add_option("-v", "--video", dest="video", help="YouTube video ID")
+    optparser.add_option("--chunk-size", dest="chunk_size")
     
     (opts, _) = optparser.parse_args()
     
     if opts.video is None:
         print("Please provide a YouTube video ID")
         exit()
+    if opts.chunk_size is not None:
+        CHUNK_SIZE = opts.chunks_size
 
 # Get the Subtitles from the YouTube video
 print("Getting subtitles...")
 video_subs = getSubsText(opts.video)
 
 chunker_init = LangChainChunker(video_subs)
-chunks = chunker_init.chunker(size=200)
+chunks = chunker_init.chunker(size=CHUNK_SIZE)
 chunk_len = len(chunks)
 print(f"subtitles divided to {chunk_len} chunks")
 
