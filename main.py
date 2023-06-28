5d00ca153bbf2e3b2382028fb1a379e8300d744c
saicharankandukuri <saicharankandukuri1x1@gmail.com>
2023-06-28 18:15:47 +0530

    main.py: fixes and changes
    - run all action with function
    - import model modules after options parsing for quick response
    - add markdown formats from main

--- main.py
+++ main.py
@@ -1,19 +1,51 @@
 import optparse, copy
-from utils.subtitles import getSubsText
-from models.distilbart_cnn_12_6 import summarize
-from models.t5_small_medium_title_generation import t5model as generate_title
-from utils.marp_wrapper import marp
-from utils.chunk import LangChainChunker
-from utils.ppt import generate_ppt
 
 CHUNK_SIZE  =   512
 VIDEO_ID    =   ""
 OUT_PPT_NAME=   "out.pptx"
 
-# Intermediary Markdown file
-print("Creating Markdown file...")
-md = marp("summary.md")
-md.header()
+def run():
+    from utils.subtitles import getSubsText
+    from models.distilbart_cnn_12_6 import summarize
+    from models.t5_small_medium_title_generation import t5model as generate_title
+    from utils.marp_wrapper import marp
+    import utils.markdown as md
+    from utils.chunk import LangChainChunker
+    from utils.ppt import generate_ppt
+    
+    # Intermediary Markdown file
+    print("Creating Markdown file...")
+    ppt = marp("summary.md")
+    ppt.add_header()
+
+    # Get the Subtitles from the YouTube video
+    print("Getting subtitles...")
+    video_subs = getSubsText(VIDEO_ID)
+
+    chunker_init    = LangChainChunker(video_subs)
+    chunks          = chunker_init.chunker(size=CHUNK_SIZE)
+    chunk_len       = len(chunks)
+
+    print(f"subtitles divided to {chunk_len} chunks")
+
+    chunk_num = 1
+    for chunk in chunks:
+        print(f"processing Chunk: {chunk_num}/{chunk_len}")
+        summary = summarize(chunk)
+        title = generate_title(summary)
+
+        ppt.add_page(
+            md.h2(title),
+            summary
+        )
+
+        chunk_num += 1
+        continue
+
+    print(f"Generating {OUT_PPT_NAME}..")
+    ppt.close_file()
+    generate_ppt("summary.md", OUT_PPT_NAME)
+
 
 if __name__ == "__main__":
     optparser = optparse.OptionParser()
@@ -34,26 +66,5 @@ if __name__ == "__main__":
     
     if opts.out_ppt_name is not None:
         OUT_PPT_NAME = optparse.out_ppt_name
-
-# Get the Subtitles from the YouTube video
-print("Getting subtitles...")
-video_subs = getSubsText(VIDEO_ID)
-
-chunker_init    = LangChainChunker(video_subs)
-chunks          = chunker_init.chunker(size=CHUNK_SIZE)
-chunk_len       = len(chunks)
-
-print(f"subtitles divided to {chunk_len} chunks")
-
-chunk_num = 1
-for chunk in chunks:
-    print(f"processing Chunk: {chunk_num}/{chunk_len}")
-    summary = summarize(chunk)
-    title = generate_title(summary)
-    md.addpage(title, summary)
-    chunk_num += 1
-    continue
-
-print(f"Generating {OUT_PPT_NAME}..")
-md.close_file()
-generate_ppt("summary.md", OUT_PPT_NAME)
+    
+    run()
