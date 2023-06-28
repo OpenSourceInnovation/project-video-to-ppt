bcb4a039175e47d66311a2d074d19debef9e941a
saicharankandukuri <saicharankandukuri1x1@gmail.com>
2023-06-28 18:14:21 +0530

    utils/marp_wrapper.py: add options for derivatives

--- utils/marp_wrapper.py
+++ utils/marp_wrapper.py
@@ -9,13 +9,15 @@ class marp:
     def marp_write(self, text):
         self.f.write(text)
 
-    def header(
+    def add_header(
         self,
         _class: str = "lead",
         paginate: bool = True,
         background: str = "#fff",
         backgroundImage: str = None,
+        extra_styles: str = None
     ):
+        ## write the header
         # ---
         # theme: gaia
         # _class: lead
@@ -31,14 +33,32 @@ class marp:
         else:
             self.marp_write(f"paginate: false\n")
         self.marp_write(f"backgroundColor: {background}\n")
-        if backgroundImage is not None:
-            self.marp_write(f"backgroundImage: url('{backgroundImage}')\n")
-        self.marp_write("---\n")
+        self.writeifNotNone(backgroundImage)
+        self.marp_end()
+        self.writeifNotNone(extra_styles) # for extra css styles
     
-    def addpage(self, title, body):
-        self.marp_write(md.h1(title))
+    def writeifNotNone(self, var):
+        if var is not None:
+            self.marp_write(var)
+    
+    def add_page(self, 
+                title=None,
+                body=None,
+                directives: str = None
+    ):
+        self.writeifNotNone(f"<!-- {directives} -->\n")
+        self.writeifNotNone(title)
+        self.writeifNotNone(body)
+        self.marp_end()
+    
+    def add_directives(self, directives: str):
+        self.marp_write(f"<!-- {directives} -->\n")
+    
+    def add_body(self, body: str):
         self.marp_write(body)
-        self.marp_write("\n\n---\n\n")
+    
+    def marp_end(self):
+        self.marp_write("\n\n---\n\n") # page end
     
     def close_file(self):
-        self.f.close()
+        self.f.close() # close the file and flush the buffer
