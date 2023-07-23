f21e23dfb757eb41225c545f76fbdd766235c98c
saicharankandukuri <saicharankandukuri1x1@gmail.com>
2023-07-23 11:46:24 +0530

    file for holding constants

---
+++ constants.py
@@ -0,0 +1,21 @@
+# to store all the generated files (*.pdf, *pptx, *webm, *mp3)
+OUTDIR="out"
+
+# path extra out files
+OUTEXTRA="out/extra"
+
+# yt video nameformat and loacation 
+YT_DEST=f"{OUTEXTRA}/vid-"
+
+# name for genrated pptx
+PPTX_DEST = f"{OUTDIR}/out.pptx"
+
+# name for generated markdown summary file
+MD_DEST = f"{OUTEXTRA}/summary.md"
+
+# name for generated png files 
+PNG_DEST = f"{OUTEXTRA}"
+
+# name for generated ppt
+PPT_DEST = f"{OUTDIR}/out.ppt"
+ 
