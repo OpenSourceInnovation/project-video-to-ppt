55523a6fa25a04397f44f8842fe5f5d7e55fb65f
saicharankandukuri <saicharankandukuri1x1@gmail.com>
2023-08-30 14:06:16 +0530

    fix typo

--- utils/mermaid.py
+++ utils/mermaid.py
@@ -1,10 +1,10 @@
 from utils.utils import Popen
 
-def mermiadImage(mmdFile: str, out="out.svg"):
-    """Convert a mermiad file to an image
+def mermaidImage(mmdFile: str, out="out.svg"):
+    """Convert a mermaid file to an image
 
     Args:
-        mmdFile (str): The mermiad file
+        mmdFile (str): The mermaid file
         out (str, optional): The output file. Defaults to "out.svg".
     """
     Popen(["mmdc", "-i", mmdFile, "-o", out])
